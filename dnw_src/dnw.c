76fb67b337be8a03ea4306ce8dbf97b113f64227
QuNengrong(Neo Qu) <Quner612@qq.com>
2013-08-21 00:45:20 +0530

    add getopt to handle options; support '-a addr' to specify the dest addr; added detailed error information to show when something goes wrong

--- dnw_src/dnw.c
+++ dnw_src/dnw.c
@@ -6,34 +6,83 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
 
 const char* dev = "/dev/secbulk0";
-#define		DOWNLOAD_ADDR			0x50008000
+static long DOWNLOAD_ADDR = 0x50008000;
+
+void showHelp();
 
 int main(int argc, char* argv[])
 {
 	unsigned char* file_buffer = NULL;
-	if( 2 != argc )	{
-		printf("Usage: dwn <filename>\n");
-		return 1;
+	char filename[128] = {0};
+	int fd = -1;                                /* if no '-f file' specified, it remains -1*/
+	int oc;
+	long addr = 0;
+
+	// handle options
+	while ((oc = getopt(argc, argv, "ha:f:")) != -1){
+		switch (oc){
+			case 'h':
+				showHelp();
+				return 0;
+
+			case 'a':
+				// addr may be hex or dec 
+				if (optarg[0]=='0' && (optarg[1]=='x'|| optarg[1]=='X')){
+					sscanf(optarg, "%x", &addr);
+					printf("get hex addr: 0x%x\n", addr);
+				}
+				else{
+					addr = atoll(optarg);
+				}
+
+				// check addr 
+				if (addr <=0 || addr>0xfffffffe){
+					printf("Please check your addr(0x%x), it may be wrong!", addr);
+					return -1;
+				}
+				else
+				{
+					DOWNLOAD_ADDR = addr;
+				}
+				break;
+
+			case 'f':
+				fd = open(optarg, O_RDONLY);
+				if(-1 == fd) {
+					printf("Can not open file - %s\n", optarg);
+					return 1;
+				}
+				memcpy(filename, optarg, sizeof(filename));
+				break;
+
+			case '?':
+			default:
+				printf("Unknow options, please see help \n");
+				showHelp();
+				return -1;
+		}
 	}
 
-	int fd = open(argv[1], O_RDONLY);
+	// no '-f file' specified
 	if(-1 == fd) {
-		printf("Can not open file - %s\n", argv[1]);
+		printf("You must specify exactly one file with -f to open\n");
+		showHelp();
 		return 1;
 	}
 
 	// get file size 
 	struct stat file_stat;
 	if( -1 == fstat(fd, &file_stat) ) {
-		printf("Get file size filed!\n");
+		perror("Get file size filed!\n");
 		return 1;
 	}	
 
 	file_buffer = (unsigned char*)malloc(file_stat.st_size+10);
 	if(NULL == file_buffer) {
-		printf("malloc failed!\n");
+		perror("malloc failed!\n");
 		goto error;
 	}
 	//memset(file_buffer, '\0', sizeof(file_buffer)); // bad code ! corrected by Qulory
@@ -41,11 +90,11 @@ int main(int argc, char* argv[])
 
 	// the first 8 bytes in the file_buffer is reserved, the last 2 bytes also;
 	if( file_stat.st_size !=  read(fd, file_buffer+8, file_stat.st_size))	{
-		printf("Read file failed!\n");
+		perror("Read file failed!\n");
 		goto error;
 	}
 
-	printf("File name : %s\n", argv[1]);
+	printf("File name : %s\n", filename);
 	printf("File size : %ld bytes\n", file_stat.st_size);// off_t is long int 
 
 	int fd_dev = open(dev, O_WRONLY);
@@ -70,6 +119,7 @@ int main(int argc, char* argv[])
 	*((unsigned short*)(file_buffer+8+file_stat.st_size)) = sum;
 
 	printf("Start Sending data...\n");
+	printf("Dest address: 0x%x\n", DOWNLOAD_ADDR);
 	size_t remain_size = file_stat.st_size+10;
 	size_t block_size = 512;
 	size_t written = 0;
@@ -78,13 +128,13 @@ int main(int argc, char* argv[])
 		size_t real_write = write(fd_dev, file_buffer+written, to_write);
 		if( to_write != real_write)	{
 			printf(" write  /dev/secbulk0 failed!  to_write = %u real_write = %u \n" , to_write ,real_write );
+			printf("Error detail: errno=%d, info:%s\n", errno, strerror(errno));
 			return 1;
 		}
 		remain_size -= to_write;
 		written += to_write;
 		printf("\rSent %lu%% \t %u bytes !", written*100/(file_stat.st_size+10),  written);
 		fflush(stdout);
-
 	}	
 
 	printf("OK\n");
@@ -103,3 +153,6 @@ error:
 	return -1;
 }
 
+void showHelp(){
+	printf("Usage: dwn [-a download_addr] <-f filename>\n");
+}
